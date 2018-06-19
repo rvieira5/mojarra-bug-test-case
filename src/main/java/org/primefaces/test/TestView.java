3b827c4e69baaf71b7bcefd80a9da6f1e8b4833b
Ramon <r_vieira5@hotmail.com>
2018-06-18 22:33:39 -0300

    Exposing bug in Mojarra implementation
    The error is reproduced by calling the method UIView#invokeOnComponent
    while the first table is being rendered. This action causes the current
    var attribute (a loop attribute) to be deleted.
    
    The error appeared with the rendering of the p:commandLink component
    because its renderer calls this method internally.

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -7,6 +7,9 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.component.ContextCallback;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
 
 import org.primefaces.test.pojo.TestObject;
 
@@ -15,6 +18,29 @@ import org.primefaces.test.pojo.TestObject;
 public class TestView implements Serializable {
 
     private List<TestObject> list;
+
+    public static class NoopCallback implements ContextCallback {
+
+        @Override
+        public void invokeContextCallback(FacesContext context, UIComponent target) {
+        }
+
+    }
+
+    public String generateOutputWithSideEffect(TestObject object) {
+        if (object == null) {
+            return null;
+        }
+
+        if (object.getFlag()) {
+            ContextCallback noopCallback = new NoopCallback();
+
+            FacesContext context = FacesContext.getCurrentInstance();
+            context.getViewRoot().invokeOnComponent(context, "thirdForm", noopCallback);
+        }
+
+        return Boolean.toString(object.getFlag());
+    }
 
     @PostConstruct
     public void init() {
