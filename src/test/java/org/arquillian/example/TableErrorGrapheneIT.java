5315377d6e37f6dc93e9957c972ef31557ea363f
Ramon <r_vieira5@hotmail.com>
2018-11-03 20:23:20 -0300

    Changing test class to use maven profile definition
    Now, the following commands can be used to run the test:
    
    mvn clean integration-test -P mojarra22
    mvn clean integration-test -P mojarra23
    mvn clean integration-test -P myfaces22
    mvn clean integration-test -P myfaces23

--- src/test/java/org/arquillian/example/TableErrorGrapheneIT.java
+++ src/test/java/org/arquillian/example/TableErrorGrapheneIT.java
@@ -2,55 +2,33 @@ package org.arquillian.example;
 
 import static org.junit.Assert.assertNotEquals;
 
-import java.io.File;
-import java.net.URL;
 import java.util.List;
 
-import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.arquillian.test.api.ArquillianResource;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.primefaces.test.TestView;
-import org.primefaces.test.pojo.TestObject;
 
 @RunWith(Arquillian.class)
 public class TableErrorGrapheneIT {
-	private static final String WEBAPP_SRC = "src/main/webapp";
 
-	@Deployment(testable = false)
-	public static WebArchive createDeployment() {
-		return ShrinkWrap.create(WebArchive.class, "mojarra-bug-test-case.war")
-				.addClasses(TestView.class, TestObject.class)
-				.addAsWebResource(new File(WEBAPP_SRC, "test.xhtml"))
-				.addAsWebResource(new File(WEBAPP_SRC, "WEB-INF/faces-config.xml"))
-				.addAsWebResource(new File(WEBAPP_SRC, "WEB-INF/web.xml"));
-	}
+    @Drone
+    private WebDriver browser;
 
-	@Drone
-	private WebDriver browser;
+    @Test
+    @RunAsClient
+    public void tableVarDataShouldNotBeErasedAfterSearch() {
+        browser.get("http://localhost:8081/mojarra-bug-test-case/test.xhtml");
 
-	@FindBy(id = "firstForm:firstTable")
-	private WebElement contentTable;
-
-	@ArquillianResource
-	private URL deploymentUrl;
-
-	@Test
-	public void tableVarDataShouldNotBeErasedAfterSearch() {
-		browser.get(deploymentUrl.toExternalForm() + "test.xhtml");
-
-		List<WebElement> tableCells = contentTable.findElements(By.xpath("//tr/td"));
-		for (WebElement tableCell : tableCells) {
-			assertNotEquals(tableCell.getText(), "--");
-		}
-	}
+        WebElement contentTable = browser.findElement(By.id("firstForm:firstTable"));
+        List<WebElement> tableCells = contentTable.findElements(By.xpath("//tr/td"));
+        for (WebElement tableCell : tableCells) {
+            assertNotEquals(tableCell.getText(), "--");
+        }
+    }
 
 }
