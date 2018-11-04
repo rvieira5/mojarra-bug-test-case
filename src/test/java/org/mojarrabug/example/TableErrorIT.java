9690ad30b1f9aeb0db5815580c7ff19becff98f4
Ramon <r_vieira5@hotmail.com>
2018-11-03 21:52:27 -0300

    Creating automated test to detect the error

---
+++ src/test/java/org/mojarrabug/example/TableErrorIT.java
@@ -0,0 +1,34 @@
+package org.mojarrabug.example;
+
+import static org.junit.Assert.assertNotEquals;
+
+import java.util.List;
+
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+@RunWith(Arquillian.class)
+public class TableErrorIT {
+
+    @Drone
+    private WebDriver browser;
+
+    @Test
+    @RunAsClient
+    public void tableVarDataShouldNotBeErasedAfterSearch() {
+        browser.get("http://localhost:8081/mojarra-bug-test-case/test.xhtml");
+
+        WebElement contentTable = browser.findElement(By.id("firstForm:firstTable"));
+        List<WebElement> tableCells = contentTable.findElements(By.xpath("//tr/td"));
+        for (WebElement tableCell : tableCells) {
+            assertNotEquals(tableCell.getText(), "--");
+        }
+    }
+
+}
